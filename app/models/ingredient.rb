2ab1da9dfa0ca245ea5c06c848858438315c3187
Johannes Vaisfeld <jvaisfeld@gmail.com>
2017-08-17 12:25:26 +0200

    models

---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+class Ingredient < ApplicationRecord
+  validates :name, uniqueness: true, presence: true, allow_blank: false
+  has_many :doses
+  has_many :cocktails, through: :doses
+  validates_associated :doses
+end
