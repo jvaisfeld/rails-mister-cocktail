24ee404afad33c240176c9547a6a6194bd91d207
Johannes Vaisfeld <jvaisfeld@gmail.com>
2017-08-17 16:23:54 +0200

    cocktail app

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,36 @@
+class DosesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.save
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+private
+def dose_params
+  params.require(:dose).permit(:description, :ingredient_id)
+end
+end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
   validates :name, uniqueness: true, presence: true, allow_blank: false
-  has_many :doses
+  has_many :doses, dependent: :destroy
   has_many :cocktails, through: :doses
   validates_associated :doses
 end
